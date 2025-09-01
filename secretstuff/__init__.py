fa0a1b316659cc914cd601b6f09bda2027fcb8cb
adw777 <aman.dogra@thewasserstoff.com>
2025-09-01 16:07:35 +0530

    minorFixes

--- secretstuff/__init__.py
+++ secretstuff/__init__.py
@@ -12,9 +12,9 @@ from .api.pipeline import SecretStuffPipeline
 from .config.labels import DEFAULT_LABELS
 from .config.dummy_values import DEFAULT_DUMMY_VALUES
 
-__version__ = "1.0.0"
-__author__ = "SecretStuff Team"
-__email__ = "info@secretstuff.com"
+__version__ = "0.0.1"
+__author__ = "axondendrite"
+__email__ = "amandogra2016@gmail.com"
 
 __all__ = [
     "PIIIdentifier",
